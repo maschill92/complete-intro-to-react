2d89ff87b6acc27052874c9b1aef8ae8a290eb30
Michael Schilling <maschill92@gmail.com>
2018-03-12 09:05:27 -0500

    Styles, dev server, routing

---
+++ js/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import preload from '../data.json';
+
+const Search = () => (
+  <div className="search">
+    {preload.shows.map(show => <h3>{show.title}</h3>)}
+  </div>
+);
+
+export default Search;
